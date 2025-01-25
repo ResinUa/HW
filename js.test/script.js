1ccae0104628c662f0b628374397fcc8e5d4c73a
ResinUa <sonyechkozoti@gmail.com>
2025-01-25 22:00:08 +0100

    js fix

--- js.test/script.js
+++ js.test/script.js
@@ -2,9 +2,9 @@ const button = document.getElementById('haii');
 
 button.addEventListener('click', function showMassage() {
     
-    let haii = prompt('Are you cutie?');
+    let haii = prompt('Are you cutie?').trim();
     
-    if (haii === 'yes', 'yes:3', ':3') {  
+    if (haii === 'yes' || haii === 'yes:3' || haii === ':3' || haii === 'sure' || haii === 'ofc') {  
         alert('Haii:3:3');
     } else {
         alert('Think again');
