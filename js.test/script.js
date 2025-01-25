d1bab2a2ebae3014b9cde228024422d455e08fef
ResinUa <sonyechkozoti@gmail.com>
2025-01-25 21:54:27 +0100

    js.test

---
+++ js.test/script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('haii');
+
+button.addEventListener('click', function showMassage() {
+    
+    let haii = prompt('Are you cutie?');
+    
+    if (haii === 'yes', 'yes:3', ':3') {  
+        alert('Haii:3:3');
+    } else {
+        alert('Think again');
+    }
+});
