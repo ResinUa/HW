ddf8a81802509ec20316b810471e6c1c78e5629a
ResinUa <sonyechkozoti@gmail.com>
2025-09-27 15:29:50 +0200

    omg I did it

--- Anemone/script.js
+++ Anemone/script.js
@@ -1,64 +1,50 @@
-// const refs = {
-//   open1: document.querySelector(".btn-open"),
-//   close1: document.querySelector(".btn-close"),
-//   bd1: document.querySelector(".backdrop"),
+const backdrop1 = document.querySelector('.backdrop');
+const backdrop2 = document.querySelector('.backdrop-second');
+const backdrop3 = document.querySelector('.backdrop-third');
 
-//   open2: document.querySelector(".btn-open-second"),
-//   close2: document.querySelector(".btn-close-second"),
-//   bd2: document.querySelector(".backdrop-second"),
+const open1 = document.querySelector('.btn-open');
+const close1 = document.querySelector('.btn-close');
+const open2 = document.querySelector('.btn-open-second');
+const close2 = document.querySelector('.btn-close-both');
+const open3 = document.querySelector('.btn-open-third');
+const close3 = document.querySelector('.btn-close-third');
 
-//   // open3: document.querySelector("btn-open-third"),
-//   // close3: document.querySelector("btn-close-third"),
-//   // bd3: document.querySelector("backdrop-third"),
+const noscrollforyou = () => {
+    document.body.style.overflow = 'hidden';
+};
 
-//   body: document.body,
-// };
+const scroll = () => {
+    document.body.style.overflow = 'auto';
+}
 
-// function openModal(el) {
-//   el.classList.remove("is-hidden");
-//   refs.body.style.overflow = "hidden";
-// }
+open1.addEventListener('click', () => {
+    backdrop1.classList.remove('is-hidden');
+    noscrollforyou();
+});
 
-// // function openModal(el) {
-// //   el.classList.remove("is-hidden-second");
-// //   refs.body.style.overflow = "hidden";
-// // }
+close1.addEventListener('click', () => {
+    backdrop1.classList.add('is-hidden');
+    scroll();
+});
 
-// function closeModal(el) {
-//   el.classList.add('is-hidden');
-//   if (refs.bd1.classList.contains('is-hidden') && refs.bd2.classList.contains('is-hidden')) {
-//     refs.body.style.overflow = '';
-//   }
-// }
+open2.addEventListener('click', () => {
+    backdrop2.classList.remove('is-hidden');
+    backdrop1.classList.add('is-hidden');
+    backdrop1.style.zIndex = '1';
+});
 
-// // function closeModal(el) {
-// //   el.classList.add("is-hidden-second");
-// //   if (
-// //     refs.bd3.classList.contains("is-hidden-second")
-// //   ) {
-// //     refs.body.style.overflow = "";
-// //   }
-// // }
+close2.addEventListener('click', () => {
+    backdrop2.classList.add('is-hidden');
+    backdrop1.style.zIndex = '3';
+    scroll();
+});
 
-// refs.open1.addEventListener("click", () => openModal(refs.bd1));
-// refs.close1.addEventListener("click", () => closeModal(refs.bd1));
+open3.addEventListener('click', () => {
+    backdrop3.classList.remove('is-hidden-second');
+    noscrollforyou();
+});
 
-// refs.open2.addEventListener("click", (e) => {
-//   e.stopPropagation();
-//   openModal(refs.bd2);
-// });
-// refs.close2.addEventListener("click", () => closeModal(refs.bd2));
-
-// [refs.bd1, refs.bd2].forEach((bd) => {
-//   bd.addEventListener("click", (e) => {
-//     if (e.target === e.currentTarget) closeModal(bd);
-//   });
-// });
-
-// refs.close2.addEventListener("click", () => {
-//   closeModal(refs.bd1);
-//   closeModal(refs.bd2);
-// });
-
-// // refs.open3.addEventListener("click", () => openModal(refs.bd3));
-// // refs.close3.addEventListener("click", () => closeModal(refs.bd3));
+close3.addEventListener('click', () => {
+    backdrop3.classList.add('is-hidden-second');
+    scroll();
+});
