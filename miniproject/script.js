c017b88c448f36206eff8504767cf4139f454207
ResinUa <sonyechkozoti@gmail.com>
2025-10-30 18:11:50 +0100

    formated document

--- miniproject/script.js
+++ miniproject/script.js
@@ -1,12 +1,12 @@
-const backdrop1 = document.querySelector('.backdrop1');
-const backdrop2 = document.querySelector('.backdrop2');
+const backdrop1 = document.querySelector(".backdrop1");
+const backdrop2 = document.querySelector(".backdrop2");
 
-const open1 = document.querySelector('.btn-open1');
-const close1 = document.querySelector('.btn-close1');
-const open2 = document.querySelector('.btn-open2');
-const close2 = document.querySelector('.btn-close2');
+const open1 = document.querySelector(".btn-open1");
+const close1 = document.querySelector(".btn-close1");
+const open2 = document.querySelector(".btn-open2");
+const close2 = document.querySelector(".btn-close2");
 
-const is_hidden = document.querySelector('.is-hidden');
+const is_hidden = document.querySelector(".is-hidden");
 
 const noscrollforyou = () => {
   document.body.style.overflow = "hidden";
@@ -17,37 +17,37 @@ const scroll = () => {
 };
 
 open1.addEventListener("click", () => {
-    backdrop1.classList.remove("is-hidden");
-    noscrollforyou();
-    backdrop1.style.zIndex = "3";
-    is_hidden.style.zIndex = "2";
-    is_hidden.style.height = "100vh";
-    backdrop1.style.height = "100vh";
-    backdrop2.style.height = "100vh";
+  backdrop1.classList.remove("is-hidden");
+  noscrollforyou();
+  backdrop1.style.zIndex = "3";
+  is_hidden.style.zIndex = "2";
+  is_hidden.style.height = "100vh";
+  backdrop1.style.height = "100vh";
+  backdrop2.style.height = "100vh";
 });
 
 close1.addEventListener("click", () => {
-    backdrop1.classList.add("is-hidden");
-    scroll();
-    backdrop1.style.zIndex = "-1";
-    is_hidden.style.height = "0px";
-    backdrop1.style.height = "0px";
-    backdrop2.style.height = "0px";
+  backdrop1.classList.add("is-hidden");
+  scroll();
+  backdrop1.style.zIndex = "-1";
+  is_hidden.style.height = "0px";
+  backdrop1.style.height = "0px";
+  backdrop2.style.height = "0px";
 });
 
 open2.addEventListener("click", () => {
-    backdrop2.classList.remove("is-hidden");
-    backdrop1.classList.add("is-hidden");
-    noscrollforyou();
-    backdrop2.style.zIndex = "3";
-    backdrop1.style.zIndex = "-1";
+  backdrop2.classList.remove("is-hidden");
+  backdrop1.classList.add("is-hidden");
+  noscrollforyou();
+  backdrop2.style.zIndex = "3";
+  backdrop1.style.zIndex = "-1";
 });
 
 close2.addEventListener("click", () => {
-    backdrop2.classList.add("is-hidden");
-    scroll();
-    backdrop2.style.zIndex = "-1";
-    is_hidden.style.height = "0px";
-    backdrop1.style.height = "0px";
-    backdrop2.style.height = "0px";
+  backdrop2.classList.add("is-hidden");
+  scroll();
+  backdrop2.style.zIndex = "-1";
+  is_hidden.style.height = "0px";
+  backdrop1.style.height = "0px";
+  backdrop2.style.height = "0px";
 });
