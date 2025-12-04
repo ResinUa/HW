a39dcaf73841ac52732b85cd4ca92d1cff00e512
ResinUa <sonyechkozoti@gmail.com>
2025-12-04 19:28:16 +0100

    woaw sale black friday -50%~!!!!

--- HW-javascript2/script.js
+++ HW-javascript2/script.js
@@ -2,28 +2,102 @@ console.log("hello, world!");
 
 console.log("через кнопки реально важко");
 
-document.querySelector('.sigmabutton').addEventListener('click', () => {
-    var Xreg = prompt('enter the first number');
-    var Yreg = prompt('enter the second number');
-    var operator = prompt('what action would you like to do');
-    if (operator !== "+" && operator !== "-" && operator !== "*" && operator !== "/") {
-        alert('ur stupid lmao');
-        var [Xreg, Yreg, operator] = undefined;
-    }
-    if (operator == "+") {
-        alert(+Xreg + +Yreg);
-        var [Xreg, Yreg, operator] = undefined;
-    }
-    if (operator == "-") {
-        alert(+Xreg - +Yreg);
-        var [Xreg, Yreg, operator] = undefined;
-    }
-    if (operator == "*") {
-        alert(+Xreg * +Yreg);
-        var [Xreg, Yreg, operator] = undefined;
-    }
-    if (operator == "/") {
-        alert(+Xreg / +Yreg);
-        var [Xreg, Yreg, operator] = undefined;
-    }
+// document.querySelector('.sigmabutton').addEventListener('click', () => {
+//     var Xreg = prompt('enter the first number');
+//     var Yreg = prompt('enter the second number');
+//     var operator = prompt('what action would you like to do');
+//     if (operator !== "+" && operator !== "-" && operator !== "*" && operator !== "/") {
+//         alert('ur stupid lmao');
+//         var [Xreg, Yreg, operator] = undefined;
+//     }
+//     if (operator == "+") {
+//         alert(+Xreg + +Yreg);
+//         var [Xreg, Yreg, operator] = undefined;
+//     }
+//     if (operator == "-") {
+//         alert(+Xreg - +Yreg);
+//         var [Xreg, Yreg, operator] = undefined;
+//     }
+//     if (operator == "*") {
+//         alert(+Xreg * +Yreg);
+//         var [Xreg, Yreg, operator] = undefined;
+//     }
+//     if (operator == "/") {
+//         alert(+Xreg / +Yreg);
+//         var [Xreg, Yreg, operator] = undefined;
+//     }
+// });
+
+var digit1 = "1";
+var digit2 = "2";
+var digit3 = "3";
+var digit4 = "4";
+var digit5 = "5";
+var digit6 = "6";
+var digit7 = "7";
+var digit8 = "8";
+var digit9 = "9";
+var digit0 = "0";
+
+var opPlus = false;
+var opMin = false;
+var opMult = false;
+var opDiv = false;
+
+var calculation = undefined;
+var Xreg = undefined;
+var Yreg = undefined;
+
+document.getElementById('calc-plus').addEventListener('click', () => {
+    var opPlus = true;
 });
+
+document.getElementById('calc-minus').addEventListener('click', () => {
+    var opMin = true;
+});
+
+document.getElementById('calc-multip').addEventListener('click', () => {
+    var opMult = true;
+});
+
+document.getElementById('calc-divis').addEventListener('click', () => {
+    var opDiv = true;
+});
+
+document.getElementById('calc-reset').addEventListener('click', () => {
+    var calculation = undefined;
+});
+
+document.getElementById('cd-1').addEventListener('click', () => {
+    if (opPlus = true) {
+    Xreg += digit1;
+    }
+    if (opPlus = true && Xreg != undefined) {
+        Yreg += digit1;
+    }
+    
+    if (opMin = true) {
+    Xreg -= digit1;
+    }
+    if (opMin = true && Xreg != undefined) {
+        Yreg -= digit1;
+    }
+    
+    if (opDiv = true) {
+    Xreg /= digit1;
+    }
+    if (opDiv = true && Xreg != undefined) {
+        Yreg /= digit1;
+    }
+    
+    if (opMult = true) {
+    Xreg *= digit1;
+    }
+    if (opMult = true && Xreg != undefined) {
+        Yreg += digit1;
+    }
+    
+    console.log(Xreg);
+    console.log(Yreg);
+});
+
---
+++ HW-new-thing-omg/script.js
@@ -0,0 +1,62 @@
+document.getElementById('ig').addEventListener('click', () => {
+    radius = prompt('radius');
+    alert(radius ** 2 * Math.PI);
+    
+    num1 = prompt('number');
+    alert(Math.abs(num1));
+    
+    a = prompt('ur stupid lmao');
+    alert(a * Math.sqrt(2));
+    
+    a1 = prompt('a in triangle');
+    h = prompt('h in triangle');
+    alert((a1 * h)/2);
+    
+    num2 = prompt('number ig');
+    alert(Math.round(num2));
+    
+    num3 = prompt('number ig down');
+    alert(Math.floor(num3));
+    
+    num41 = prompt('number omg');
+    num42 = prompt('number omg2');
+    alert(Math.max(num41, num42));
+    alert(Math.min(num41, num42));
+    
+    num5 = prompt('squaaaare');
+    alert(Math.pow(num5, 2));
+    
+    num6 = prompt('roooot');
+    alert(Math.sqrt(num6));
+    alert(Math.cbrt(num6));
+    
+    num71 = prompt('number again');
+    num72 = prompt('number again2');
+    alert(Math.pow(num71, num72));
+    
+    num81 = prompt('diagonal deez nuts');
+    num82 = prompt('diagonal deez nuts2');
+    
+    alert(Math.sqrt(num81 ** 2 + num82 ** 2))
+    alert(Math.random());
+    alert(Number.parseInt(Math.random()*10));
+    alert(Number.parseInt(Math.random()*100));
+});
+
+document.getElementById('ig2').addEventListener('click', () => {
+    num21 = Number.parseInt(Math.random()*100);
+    num212 = Number.parseInt(Math.random()*100);
+    num213 = Number.parseInt(Math.random()*100);
+    alert(Math.max(num21, num212, num213));
+    
+    thousand = 1000;
+    alert('знижка woaw');
+    alert("АЖ " + (thousand * Number.parseInt(Math.random(0.1)*0.5)))
+
+    num22 = prompt('why are we doing this again?');
+    alert("тіпа то чісло до 2 це " + Math.pow(num22, 2));
+
+    num23 = Math.random()*100;
+    num231 = Math.random()*100;
+    alert("допустім це " + Math.abs(num23 - num231));
+});
