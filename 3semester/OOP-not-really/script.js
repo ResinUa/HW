5c42e1d13a355a4dac65a76d8296cac06f6386e5
ResinUa <sonyechkozoti@gmail.com>
2026-02-28 22:17:24 +0100

    script tomorrow

---
+++ 3semester/OOP-not-really/script.js
@@ -0,0 +1,20 @@
+//------------------------------S1-events----------------------------------
+// document.querySelector(".otcthing").addEventListener("click", () => {
+//    https://youtu.be/dQw4w9WgXcQ
+// });
+//-------------------------------------------------------------------------
+document.getElementById("headerlogin").addEventListener("mousedown", () => {
+  document.getElementById("headerlogin").style.scale = "0.975";
+});
+
+document.getElementById("headerlogin").addEventListener("mouseup", () => {
+  document.getElementById("headerlogin").style.scale = "1";
+});
+
+document.getElementById("headerbuy").addEventListener("mousedown", () => {
+  document.getElementById("headerbuy").style.scale = "0.975";
+});
+
+document.getElementById("headerbuy").addEventListener("mouseup", () => {
+  document.getElementById("headerbuy").style.scale = "1";
+});
