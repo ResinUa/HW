f8d1f364a478a936a19eefdd76fe6e1ab1d0925c
ResinUa <sonyechkozoti@gmail.com>
2026-06-11 20:12:13 +0200

    I feel so sigma

--- 4semester/project/script.js
+++ 4semester/project/script.js
@@ -1,3 +1,51 @@
+//entry things
+const welcome = document.getElementById('modalwelcomebg');
+const modalinput = document.getElementById('modalinput');
+const namething = document.getElementById('namething');
+const modalthxopen = document.getElementById('modalthxopen');
+const modalwelcome = document.getElementById('modalwelcome');
+const modalthx = document.getElementById('modalthx');
+const modalthxclose = document.getElementById('modalthxclose');
+
+var user = undefined;
+
+modalinput.addEventListener('keyup', (el) => {
+    user = el.target.value;
+});
+
+modalthxopen.addEventListener('click', () => {
+    if (user == undefined) {
+        return;
+    }
+    modalwelcome.style.display = 'none';
+    modalthx.style.display = 'flex';
+});
+
+modalthxclose.addEventListener('click', () => {
+        if (user == 'tung tung tung sahur') {
+            document.getElementById('html').style.display = 'none';
+        }
+        namething.textContent = 'Вітаємо, ' + user;
+        welcome.style.display = 'none';
+        modalthx.style.display = 'none';
+})
+//header things
+//navbar interactive thing
+const navinterthing = document.getElementById('navinterthing');
+const headermodal = document.getElementById('headermodal');
+const headermodalbtn = document.querySelectorAll('#headermodal');
+
+var navinterbool = false;
+
+navinterthing.addEventListener('click', () => {
+    if (navinterbool == false) {
+        headermodal.style.display = 'flex';
+        navinterbool = true;
+    } else {
+        headermodal.style.display = 'none';
+        navinterbool = false;
+    }
+});
 //theme
 const themebtn = document.getElementById('theme');
 
