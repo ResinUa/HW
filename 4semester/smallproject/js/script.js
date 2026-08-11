0b7c7f9674c7a974a90807a2a7ddc3d8b250abbb
ResinUa <sonyechkozoti@gmail.com>
2026-08-11 20:29:27 +0200

    boku wa kimi no tsubasa ni nareru yuuki ga aru yo-

--- 4semester/smallproject/js/script.js
+++ 4semester/smallproject/js/script.js
@@ -1,56 +1,63 @@
 // yarik modal&header
 let modalbool = false;
 
-const smopen = document.getElementById('smopen');
-const smclose = document.getElementById('smclose');
+const smopen = document.getElementById("smopen");
+const smclose = document.getElementById("smclose");
 
-const searchmodal = document.getElementById('searchmodal');
+const searchmodal = document.getElementById("searchmodal");
 
-smopen.addEventListener('click', () => {
-    if (modalbool === true) {
-        return;
-    }
-    searchmodal.style.display = 'flex';
-    modalbool = true;
+smopen.addEventListener("click", () => {
+  if (modalbool === true) {
+    return;
+  }
+  searchmodal.style.display = "flex";
+  modalbool = true;
 });
 
-smclose.addEventListener('click', () => {
-    searchmodal.style.display = 'none';
-    modalbool = false;
+smclose.addEventListener("click", () => {
+  searchmodal.style.display = "none";
+  modalbool = false;
 });
 
- const lmopen = document.getElementById('lmopen');
- const lmclose = document.getElementById('lmclose');
+const lmopen = document.getElementById("lmopen");
+const lmclose = document.getElementById("lmclose");
 
- const loginmodal = document.getElementById('loginmodal');
+const loginmodal = document.getElementById("loginmodal");
 
-lmopen.addEventListener('click', () => {
-    if (modalbool === true) {
-        return;
-    }
-    loginmodal.style.display = 'flex';
-    modalbool = true;
+lmopen.addEventListener("click", () => {
+  if (modalbool === true) {
+    return;
+  }
+  loginmodal.style.display = "flex";
+  modalbool = true;
 });
 
-lmclose.addEventListener('click', () => {
-    loginmodal.style.display = 'none';
-    modalbool = false;
+lmclose.addEventListener("click", () => {
+  loginmodal.style.display = "none";
+  modalbool = false;
 });
 
- const bmopen = document.getElementById('bmopen');
- const bmclose = document.getElementById('bmclose');
+const bmopen = document.getElementById("bmopen");
+const bmclose = document.getElementById("bmclose");
 
- const bagmodal = document.getElementById('bagmodal');
+const bagmodal = document.getElementById("bagmodal");
 
-bmopen.addEventListener('click', () => {
-    if (modalbool === true) {
-        return;
-    }
-    bagmodal.style.display = 'flex';
-    modalbool = true;
+bmopen.addEventListener("click", () => {
+  if (modalbool === true) {
+    return;
+  }
+  bagmodal.style.display = "flex";
+  modalbool = true;
 });
 
-bmclose.addEventListener('click', () => {
-    bagmodal.style.display = 'none';
-    modalbool = false;
+bmclose.addEventListener("click", () => {
+  bagmodal.style.display = "none";
+  modalbool = false;
+});
+
+const ah = document.getElementById("ah");
+const hm = document.getElementById("homemodal");
+
+ah.addEventListener("mouseover", () => {
+  hm.style.animation = "hmappear 0.25s forwards";
 });
